d83f7704ec4d1ba636138bd61b87e8c7369fcb67
Adam Rice <adamcoltonrice@gmail.com>
2016-12-16 13:15:08 -0700

    add user list with styling

--- lib/components/Application.jsx
+++ lib/components/Application.jsx
@@ -62,8 +62,11 @@ export default class Application extends Component {
           <article id="active-user-list">
             <h2>Users</h2>
             <ul>
-              <li>
-                <p className="user-name"></p>
+              <li className="active-user-list-item" >
+                <p className="user-name">Andy (an@happyFINGERS.io)</p>
+              </li>
+              <li className="active-user-list-item" >
+                <p className="user-name">Billy (bg@happyFINGERS.io)</p>
               </li>
             </ul>
           </article>
