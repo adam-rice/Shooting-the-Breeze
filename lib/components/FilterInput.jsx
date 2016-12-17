6c34a7a6bd7380e470577dea4992793bb0f48e65
Adam Rice <adamcoltonrice@gmail.com>
2016-12-17 14:25:21 -0700

    bring in lodash on Filter comp

--- lib/components/FilterInput.jsx
+++ lib/components/FilterInput.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { filter } from 'lodash';
 
 export default class FilterInput extends Component {
   render() {
+    const {filterMessages} = this.props;
     return (
       <div>
       <input
