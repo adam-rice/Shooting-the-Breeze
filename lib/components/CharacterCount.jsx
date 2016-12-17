8671e11e14623da85228b405082d13fedd0979b3
Adam Rice <adamcoltonrice@gmail.com>
2016-12-16 21:25:20 -0700

    render on char count component

---
+++ lib/components/CharacterCount.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+export default class CharacterCount extends Component {
+  render() {
+    return (
+      <p id="char-count">{this.props.draftMessageProp.length}</p>
+    )
+  }
+}
