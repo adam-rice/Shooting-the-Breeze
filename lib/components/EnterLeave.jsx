3e21131be2a904e8acadd8b5c101760a5a4df438
Adam Rice <adamcoltonrice@gmail.com>
2016-12-16 23:26:39 -0700

    add sign out btn that works

--- lib/components/EnterLeave.jsx
+++ lib/components/EnterLeave.jsx
@@ -5,7 +5,15 @@ export default class EnterLeave extends Component {
   render() {
     return(
       <section id="logged-in">
-        {this.props.user ? <h3>Logged in as <span>{this.props.user.displayName}</span> ({this.props.user.email})</h3> : <button className="btn" onClick={() => this.props.signInFunction()}>Sign In</button> }
+        {this.props.user ? <div><h3>Logged in as <span>{this.props.user.displayName}</span> ({this.props.user.email})</h3>
+        <button
+          id="sign-out"
+          onClick={() => this.props.signOutFunction()}>Sign Out
+        </button></div>
+        : <button
+            className="btn"
+            onClick={() => this.props.signInFunction()}>Sign In
+          </button> }
       </section>
     )
   }
