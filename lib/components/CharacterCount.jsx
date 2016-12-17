af3c1dfdfe86635bb29ac04347c8d0974c3071ec
Adam Rice <adamcoltonrice@gmail.com>
2016-12-17 00:53:09 -0700

    add counter method to charCount component

--- lib/components/CharacterCount.jsx
+++ lib/components/CharacterCount.jsx
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 export default class CharacterCount extends Component {
+  counter() {
+    if (this.props.draftMessageProp.length === 0) {
+      return 140;
+    } else {
+      return 140-this.props.draftMessageProp.length;
+    }
+  }
+
   render() {
     return (
       <div id="char-count">
-        <p>{this.props.draftMessageProp.length}</p>
+        <p>{this.counter()}</p>
       </div>
     )
   }
