08a4372c5523bdb9c2267c14aa9b8ca2c56968e6
Adam Rice <adamcoltonrice@gmail.com>
2016-12-19 10:31:41 -0700

    add sinon to testing & passing test for FilterInput length

--- test/Application.spec.js
+++ test/Application.spec.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
-import { assert } from 'chai';
+import { assert, expect } from 'chai';
 import sinon from 'sinon';
 
 import Application from '../lib/components/Application';
+import FilterInput from '../lib/components/FilterInput';
 
 describe('Application', () => {
   it('renders as a <div>', () => {
-    const wrapper = shallow(<Application />)
+    const wrapper = shallow(<Application />);
     assert.equal(wrapper.type(), 'div');
   });
 });
 
-describe('<MyComponent />', () => {
-  it('renders three <Foo /> components', () => {
-    const wrapper = shallow(<MyComponent />);
-    expect(wrapper.find(Foo)).to.have.length(3);
+describe('<Application />', () => {
+  it('renders one <FilterInput /> component', () => {
+    const wrapper = shallow(<Application />);
+    expect(wrapper.find(FilterInput)).to.have.length(1);
   });
+});
