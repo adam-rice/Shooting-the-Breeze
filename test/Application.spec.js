07e1d6feba19ee1c66188f956de09fc717064183
Adam Rice <adamcoltonrice@gmail.com>
2016-12-19 10:25:37 -0700

    add sinon to devDeps

--- test/Application.spec.js
+++ test/Application.spec.js
@@ -1,15 +1,19 @@
 import React from 'react';
-
 import { shallow, mount, render } from 'enzyme';
 import { assert } from 'chai';
+import sinon from 'sinon';
 
 import Application from '../lib/components/Application';
 
 describe('Application', () => {
-
   it('renders as a <div>', () => {
     const wrapper = shallow(<Application />)
     assert.equal(wrapper.type(), 'div');
   });
-
 });
+
+describe('<MyComponent />', () => {
+  it('renders three <Foo /> components', () => {
+    const wrapper = shallow(<MyComponent />);
+    expect(wrapper.find(Foo)).to.have.length(3);
+  });
