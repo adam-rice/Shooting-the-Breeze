5f5fb148a71ebea3d830af52ac551d0042cd5379
Adam Rice <adamcoltonrice@gmail.com>
2016-12-20 13:47:38 -0700

    refactor existing tests and wip state change test

--- test/Application.spec.js
+++ test/Application.spec.js
@@ -9,38 +9,44 @@ import Sort from '../lib/components/Sort';
 import Messages from '../lib/components/Messages';
 import Users from '../lib/components/Users';
 
-
 describe('Application', () => {
   it('renders as a <div>', () => {
     const wrapper = shallow(<Application />);
     assert.equal(wrapper.type(), 'div');
   });
-});
 
-describe('<Application />', () => {
   it('renders one <FilterInput /> component', () => {
     const wrapper = shallow(<Application />);
     expect(wrapper.find(FilterInput)).to.have.length(1);
   });
-});
 
-describe('<Application />', () => {
   it('renders one <Sort /> component', () => {
     const wrapper = shallow(<Application />);
     expect(wrapper.find(Sort)).to.have.length(1);
   });
-});
 
-describe('<Application />', () => {
   it('renders one <Messages /> component', () => {
     const wrapper = shallow(<Application />);
     expect(wrapper.find(Messages)).to.have.length(1);
   });
-});
 
-describe('<Application />', () => {
   it('renders one <Users /> component', () => {
     const wrapper = shallow(<Application />);
     expect(wrapper.find(Users)).to.have.length(1);
   });
+
+it.skip('should change state if I add ideas to the dom', () => {
+    const wrapper = mount(<Main/>);
+    const title = wrapper.find('.CreateIdea-title');
+    title.simulate('change', title.node.value = 'testing title');
+
+    const body = wrapper.find('.CreateIdea-body');
+   body.simulate('change', body.node.value = 'testing body');
+   wrapper.find('.CreateIdea-submit').simulate('click');
+
+   expect(wrapper.state().ideas.length).to.equal(1);
+   expect(wrapper.state().ideas[0].title).to.equal('testing title');
+   expect(wrapper.state().ideas[0].title).to.equal('testing title');
+  });
+
 });
