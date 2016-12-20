9e329f2da7688e9148950a4111767de3fd1ee018
Adam Rice <adamcoltonrice@gmail.com>
2016-12-20 16:18:56 -0700

    read me update

--- test/Messages-test.js
+++ test/Messages-test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { assert } from 'chai';
+import { assert, expect } from 'chai';
 import { map } from 'lodash';
 
 import Messages from '../lib/components/Messages';
