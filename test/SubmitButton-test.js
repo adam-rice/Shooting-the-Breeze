8b57eb806777264105472c39b9518bcf77ee44b2
Adam Rice <adamcoltonrice@gmail.com>
2016-12-20 17:03:28 -0700

    wip submit btn component testing

---
+++ test/SubmitButton-test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import { assert, expect } from 'chai';
+import Locus from 'locus';
+import sinon from 'sinon';
+
+import SubmitButton from '../lib/components/SubmitButton';
